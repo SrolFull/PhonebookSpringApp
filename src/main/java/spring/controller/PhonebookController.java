d33a01f54b01571b0c0226d51d10c50413a5ba4e
SrolFull <srolfull@gmail.com>
2021-11-25 20:43:05 +0500

    *change RestController to Controller *Modify getClients: added pagination *Added jsp pages for all controller methods

--- src/main/java/spring/controller/PhonebookController.java
+++ src/main/java/spring/controller/PhonebookController.java
@@ -1,49 +1,71 @@
 package spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 import spring.models.Client;
+import org.springframework.data.domain.Page;
 import spring.service.PhonebookService;
 
-import java.util.List;
+import javax.naming.Binding;
+import java.util.Map;
+import java.util.Optional;
 
-@RestController
+@Controller
 public class PhonebookController {
     @Autowired
     PhonebookService phonebookService;
 
-    @PutMapping("/addMember")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Client addMember(@RequestBody Client client){
-        return phonebookService.addMember(client);
+    @GetMapping("/addClient")
+    public ModelAndView showForm(){
+        return new ModelAndView("AddMemberPage","client",new Client());
+    }
+
+    @PostMapping("/submit")
+    public ModelAndView submit(@ModelAttribute("client") Client client, BindingResult result, ModelMap modelMap){
+        ModelAndView model = new ModelAndView("AddMemberPage","client",new Client());
+        phonebookService.addMember(client);
+        return model;
     }
 
     @PutMapping("/addMembers")
-    @ResponseStatus(HttpStatus.CREATED)
-    public List<Client> addMembers(@RequestBody List<Client> clients){
-        return phonebookService.addMembers(clients);
+    @ResponseStatus(HttpStatus.OK)
+    public String addMembers(Model model){
+        return null;
     }
 
     @GetMapping("/deleteMemberById")
     @ResponseStatus(HttpStatus.OK)
-    public Client  deleteMemberById(@RequestParam Integer id){
-        return phonebookService.deleteMember(id);
+    public String  deleteMemberById(Model model){
+        return null;
     }
 
     @GetMapping("/deleteMemberByName")
-    public Client deleteMemberByName(@RequestParam String firstName, @RequestParam String lastName){
-        return phonebookService.deleteMember(firstName, lastName);
+    public String deleteMemberByName(Model model){
+        return null;
     }
 
     @GetMapping("/deleteMembersById")
-    public List<Client> deleteMembersById(@RequestParam List<Integer> listId){
-        return phonebookService.deleteMembers(listId);
+    public String deleteMembersById(Model model){
+        return null;
     }
 
     @GetMapping("/getClients")
-    public List<Client> getAllClients(){
-        return phonebookService.getAllClients();
+    public String getAllClients(@RequestParam(name = "page",defaultValue = "0") Integer pageNumber, Model model){
+        Pageable pgable = PageRequest.of(pageNumber, 10);
+        Page<Client> clientPage = phonebookService.getPageClients(pgable);
+        model.addAttribute("clientList", clientPage.getContent());
+        model.addAttribute("currentPage", pageNumber);
+        model.addAttribute("totalPages", clientPage.getTotalPages());
+        return "MembersList";
     }
 }
--- src/main/java/spring/service/PhonebookService.java
+++ src/main/java/spring/service/PhonebookService.java
@@ -1,6 +1,9 @@
 package spring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.models.Client;
@@ -47,6 +50,10 @@ public class PhonebookService {
 
     public List<Client> getAllClients(){
         return phonebookRepository.findAll();
+    }
+
+    public Page<Client> getPageClients(Pageable page){
+        return phonebookRepository.findAll(page);
     }
 
 }
